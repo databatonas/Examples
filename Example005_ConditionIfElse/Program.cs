758ce5934751a041b3adb7f45513fa019b2bdcad
Yuriy <databatonas@gmail.com>
2023-01-26 20:02:29 +0300

    Ввод имени

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,17 @@
 ﻿Console.Write(" Введите имя пользователя: ");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();/*?- означает, что значение может быть и NALL.*/
 
-if(username.ToLower() == "маша")
+if (username != null)/* Если username не равно nall? то...*/
 {
-    Console.WriteLine(" Ура, это же МАША! ");
-}
-else
-{
-    Console.Write(" Привет, ");
-    Console.WriteLine(username);
+    if (username.ToLower() == "маша")/*Перевод букв в верхний регистр. 
+                                       Если имя "Маша" ввели любыми буквами 
+                                       (на русском языке), то...*/
+    {
+        Console.WriteLine(" Ура, это же МАША! ");
+    }
+    else
+    {
+        Console.Write(" Привет, ");
+        Console.WriteLine(username);
+    }
 }
