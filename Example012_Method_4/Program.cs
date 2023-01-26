eb3219dba7cff37509abbc8cdc0647623fbbdfd6
Yuriy <databatonas@gmail.com>
2023-01-26 12:39:43 +0300

    Метод 4

---
+++ Example012_Method_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Четвёртая группа методов, которые что-то принимают и что-то возвращают.*/
+
+string Method4(int count, string text)/* Выведем строку (text) count раз.*/
+{
+    int i = 0;
+    string result = string.Empty;/* string.Empty- пустая строка (как ноль). 
+                                    Вместо string.Empty можно ставить ("").*/
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;  /* result- это та переменная, значение которой мы хотим получить из метода.*/
+}
+
+/*Вызов метода...*/
+
+/* Создадим переменную res.*/
+string res = Method4(10, "какой-то текст "); /* На экране будет 10 раз "какой-то текст".*/
+Console.WriteLine(res);
