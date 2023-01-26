b1ece2fc9f585a03bfdeac864b0e22d568d08d1d
Юрий Владимирович Пакшинцев <116585108+databatonas@users.noreply.github.com>
2023-01-26 18:51:16 +0300

    Delete Exammple002_HelloUser directory

--- Exammple002_HelloUser/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.WriteLine("Введите ваше имя");
-string? username = Console.ReadLine(); // Вводим имя. ?- это значит отличный от NULL
-Console.WriteLine($"Привет, {username} !"); // Выводим имя на консоль
