cbf6f0acba0b2e0e54cf835f9e01aafcd00d7fa5
Yuriy <databatonas@gmail.com>
2023-01-26 18:49:30 +0300

    Исправил название

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Введите ваше имя");
+string? username = Console.ReadLine(); // Вводим имя. ?- это значит отличный от NULL
+Console.WriteLine($"Привет, {username} !"); // Выводим имя на консоль
