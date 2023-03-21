1b5850a1866745fa9771c99d39d10f1eb0fc9562
Yuriy <databatonas@gmail.com>
2023-03-21 19:08:11 +0300

    Зарисовка объекта

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -3,7 +3,7 @@
 //Console.WriteLine("+");
 
 int xa = 40, ya = 1,
-    xb = 1, yb = 25,
+    xb = 1,  yb = 25,
     xc = 80, yc = 25;
 
 Console.SetCursorPosition(xa, ya);
@@ -17,29 +17,29 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while( count < 10000 )
+while (count < 10000)
 {
-    int what = new Random().Next(0,3); // [0;3) 0 1 2
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
 
-{
-    x = (x + xa) / 2;
-    y = (y + ya) / 2;
-}
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
 
-if(what == 1)
-{
-    x = (x + xb) / 2;
-    y = (y + yb) / 2;
-}
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
 
-if(what == 2) 
-{
-    x = (x + xc) / 2;
-    y = (y + yc) / 2;
-}
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
 
-Console.SetCursorPosition(x, y);
-Console.WriteLine("+");
-count++;
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
 }
 
