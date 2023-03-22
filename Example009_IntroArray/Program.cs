acae0be7a65827759a7b373105c6f2b2123f6445
Yuriy <databatonas@gmail.com>
2023-03-22 18:15:56 +0300

    Макcимум из 9 чисел массива

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -4,7 +4,7 @@ int Max(int arg1, int arg2, int arg3)/* назначаем три числа и 
 int result = arg1; /* принимаем arg1 за максимальное число.*/
 if(arg2 > result) result = arg2;
 if(arg3 > result) result = arg3;
-return result; /* возвращаем результат*/
+return result; /* возвращаем результат.*/
 }
 // индексы      0  1  2  3  4  5  6  7  8
 int [] array = {21,22,23,24,25,26,27,28,29};
