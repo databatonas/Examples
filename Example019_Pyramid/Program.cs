6b54953e7b6eb3dd4d0bb55e014f1755a44481e9
Yuriy <databatonas@gmail.com>
2023-03-24 18:06:59 +0300

    Игра в пирамидки

---
+++ Example019_Pyramid/Program.cs
@@ -0,0 +1,11 @@
+﻿// Игра в пирамидки.
+/* wite- шпиль, который является рабочим, откуда мы будем брать текущий блинчик. 
+on- шпиль, на котором пирамидка должна оказаться. some- промежуточный шпиль. 
+count- количество блинов у нас всего есть (их можно задавать пользователю). Towers- любое название метода.*/
+void Towers(string wite = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(wite, some, on, count - 1);
+    Console.WriteLine($"{wite} >>{on}");
+    if (count > 1) Towers(some, on, wite, count - 1);
+}
+Towers();
