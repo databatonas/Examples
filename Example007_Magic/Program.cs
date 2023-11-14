01b4d0d97628acb284cbb6607a2125cae4d28dc9
Yuriy <databatonas@gmail.com>
2023-11-14 21:10:42 +0300

    Редактирование программы

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -3,7 +3,7 @@
 //Console.WriteLine("+");
 
 int xa = 40, ya = 1,
-    xb = 1,  yb = 25,
+    xb = 1, yb = 25,
     xc = 80, yc = 25;
 
 Console.SetCursorPosition(xa, ya);
@@ -21,6 +21,7 @@ while (count < 10000)
 {
     int what = new Random().Next(0, 3); // [0;3) 0 1 2
 
+    if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
